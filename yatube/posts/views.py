7e60d25e6bad53ac8a74fe7dba708e08441ba0ea
bissaliev <bissaliev21@gmail.com>
2024-02-17 19:28:05 +0400

    верстка 2

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,4 +1,9 @@
 from django.contrib.auth.decorators import login_required
+from django.http import (
+    HttpResponse,
+    HttpResponsePermanentRedirect,
+    HttpResponseRedirect,
+)
 from django.shortcuts import get_object_or_404, redirect, render
 from django.views.decorators.cache import cache_page
 
@@ -7,95 +12,84 @@ from .models import Comment, Follow, Group, Post, User
 from .utils import get_page_context
 
 
-@cache_page(20, key_prefix='index_page')
+@cache_page(20, key_prefix="index_page")
 def index(request):
-    """ Функция представления главной страницы."""
-    posts_list = Post.objects.select_related('author', 'group').all()
+    """Функция представления главной страницы."""
+    posts_list = Post.objects.select_related("author", "group").all()
     context = get_page_context(posts_list, request)
-    return render(request, 'posts/index.html', context)
+    return render(request, "posts/index.html", context)
 
 
-def group_posts(request, slug):
-    """ Функция представления страницы группы."""
+def group_posts(request, slug) -> HttpResponse:
+    """Функция представления страницы группы."""
     group = get_object_or_404(Group, slug=slug)
-    posts_list = group.posts.select_related('author', 'group').all()
+    posts_list = group.posts.select_related("author", "group").all()
     context = {
-        'group': group,
+        "group": group,
     }
-    context.update(get_page_context(posts_list, request))
-    return render(request, 'posts/group_list.html', context)
+    context |= get_page_context(posts_list, request)
+    return render(request, "posts/group_list.html", context)
 
 
 def profile(request, username):
-    """ Функция представления страницы профиля автора."""
+    """Функция представления страницы профиля автора."""
     author = get_object_or_404(User, username=username)
     posts = author.posts.all()
     posts_count = posts.count()
     following = Follow.objects.filter(author=author).exists()
-    context = {
-        'author': author,
-        'posts_count': posts_count,
-        'following': following
-    }
-    context.update(get_page_context(posts, request))
-    return render(request, 'posts/profile.html', context)
+    context = {"author": author, "posts_count": posts_count, "following": following}
+    context |= get_page_context(posts, request)
+    return render(request, "posts/profile.html", context)
 
 
 def post_detail(request, post_id):
-    """ Функция представления страницы информации поста."""
+    """Функция представления страницы информации поста."""
     post = get_object_or_404(Post, pk=post_id)
     count_author = post.author.posts.count()
     comments = Comment.objects.select_related(
-        'post',
+        "post",
     )
     form = CommentForm(request.POST or None)
     context = {
-        'post': post,
-        'count_author': count_author,
-        'form': form,
-        'comments': comments
+        "post": post,
+        "count_author": count_author,
+        "form": form,
+        "comments": comments,
     }
-    return render(request, 'posts/post_detail.html', context)
+    return render(request, "posts/post_detail.html", context)
 
 
 @login_required
 def post_create(request):
-    """ Функция представления страницы создания поста."""
+    """Функция представления страницы создания поста."""
     form = PostForm(request.POST or None, files=request.FILES or None)
-    if request.method == 'POST' and form.is_valid():
+    if request.method == "POST" and form.is_valid():
         post = form.save(commit=False)
         post.author = request.user
         form.save()
-        return redirect('posts:profile', request.user.username)
+        return redirect("posts:profile", request.user.username)
     form = PostForm()
-    return render(request, 'posts/create_post.html', {'form': form})
+    return render(request, "posts/create_post.html", {"form": form})
 
 
 @login_required
 def post_edit(request, post_id):
-    """ Функция представления страницы редактирования поста."""
+    """Функция представления страницы редактирования поста."""
     is_edit = True
     post = get_object_or_404(Post, pk=post_id)
-    form = PostForm(
-        request.POST or None,
-        files=request.FILES or None,
-        instance=post
-    )
-    if request.method == 'POST' and form.is_valid():
+    form = PostForm(request.POST or None, files=request.FILES or None, instance=post)
+    if request.method == "POST" and form.is_valid():
         post = form.save(commit=False)
         post.author = request.user
         form.save()
-        return redirect('posts:post_detail', post_id)
+        return redirect("posts:post_detail", post_id)
     form = PostForm(instance=post)
-    return render(
-        request, 'posts/create_post.html',
-        {'form': form, 'is_edit': is_edit}
-    )
+    return render(request, "posts/create_post.html", {"form": form, "is_edit": is_edit})
 
 
 @login_required
 def add_comment(request, post_id):
-    """ Функция добавления комментария."""
+    """Функция добавления комментария."""
     post = get_object_or_404(Post, pk=post_id)
     form = CommentForm(request.POST or None)
     if form.is_valid():
@@ -103,26 +97,28 @@ def add_comment(request, post_id):
         comment.author = request.user
         comment.post = post
         comment.save()
-    return redirect('posts:post_detail', post_id=post_id)
+    return redirect("posts:post_detail", post_id=post_id)
 
 
 @login_required
-def follow_index(request):
+def follow_index(request) -> HttpResponse:
     """Функция представления постов избранных авторов."""
     posts_list = Post.objects.filter(author__following__user=request.user)
     context = get_page_context(posts_list, request)
-    return render(request, 'posts/follow.html', context)
+    return render(request, "posts/follow.html", context)
 
 
 @login_required
-def profile_follow(request, username):
+def profile_follow(
+    request, username
+) -> HttpResponseRedirect | HttpResponsePermanentRedirect:
     """Функция подписки на автора."""
     user = request.user
     author = get_object_or_404(User, username=username)
     if user.follower.filter(author=author).exists() or user == author:
-        return redirect('posts:profile', username=username)
+        return redirect("posts:profile", username=username)
     Follow.objects.create(user=user, author=author)
-    return redirect('posts:profile', username=username)
+    return redirect("posts:profile", username=username)
 
 
 @login_required
@@ -131,4 +127,4 @@ def profile_unfollow(request, username):
     user = request.user
     author = get_object_or_404(User, username=username)
     Follow.objects.filter(user=user, author=author).delete()
-    return redirect('posts:profile', username=username)
+    return redirect("posts:profile", username=username)
