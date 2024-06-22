fa4906c415d21da5523fe23666eb70f8224b8a2c
bissaliev <bissaliev21@gmail.com>
2024-06-22 10:34:35 +0400

    Add func delete_image_on_model in posts/signals.py for delete image before delete post

--- yatube/posts/signals.py
+++ yatube/posts/signals.py
@@ -1,4 +1,4 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_delete, post_save
 from django.dispatch import receiver
 from posts.models import Post
 
@@ -6,3 +6,9 @@ from posts.models import Post
 @receiver(post_save, sender=Post)
 def post_save_post(sender, instance, created, update_fields, **kwargs) -> None:
     instance.update_search_vector()
+
+
+@receiver(post_delete, sender=Post)
+def delete_image_on_model(sender, instance, **kwargs):
+    if instance.image:
+        instance.image.delete(False)
