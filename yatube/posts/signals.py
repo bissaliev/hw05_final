ca9b796e4b668c4dc0195d3a1e43efea54391a43
bissaliev <bissaliev21@gmail.com>
2024-06-22 19:58:03 +0400

    thumbnail

--- yatube/posts/signals.py
+++ yatube/posts/signals.py
@@ -1,6 +1,13 @@
+import contextlib
+import os
+
+from django.conf import settings
 from django.db.models.signals import post_delete, post_save
 from django.dispatch import receiver
 from posts.models import Post
+from sorl.thumbnail import delete as thumbnail_delete
+from sorl.thumbnail import get_thumbnail
+from sorl.thumbnail.models import KVStore
 
 
 @receiver(post_save, sender=Post)
@@ -11,4 +18,7 @@ def post_save_post(sender, instance, created, update_fields, **kwargs) -> None:
 @receiver(post_delete, sender=Post)
 def delete_image_on_model(sender, instance, **kwargs):
     if instance.image:
+        # kvstore_entries = KVStore.objects.filter(value__icontains=instance.image.name)
+        # print(kvstore_entries.__dict__)
+        thumbnail_delete(instance.image)
         instance.image.delete(False)
