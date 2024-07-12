9517a8c48e2ade2d2a7f1ee6b87af2f90561704b
bissaliev <bissaliev21@gmail.com>
2024-07-12 15:07:59 +0400

    Рефакторинг core/views.py

--- yatube/core/views.py
+++ yatube/core/views.py
@@ -2,12 +2,12 @@ from django.shortcuts import render
 
 
 def page_not_found(request, exception):
-    return render(request, 'core/404.html', {'path': request.path}, status=404)
+    return render(request, "core/404.html", {"path": request.path}, status=404)
 
 
-def csrf_failure(request, reason=''):
-    return render(request, 'core/403csrf.html')
+def csrf_failure(request, reason=""):
+    return render(request, "core/403csrf.html")
 
 
 def server_error(request):
-    return render(request, 'core/500.html', status=500)
+    return render(request, "core/500.html", status=500)
