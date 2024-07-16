63223067a86ff5588679b24fa441b86edd7f6195
bissaliev <bissaliev21@gmail.com>
2024-07-16 20:28:04 +0400

    Добавление классов представлений лоя редактирования и списка пользователей(UserUpdateView, UserListView)

--- yatube/users/views.py
+++ yatube/users/views.py
@@ -1,7 +1,8 @@
 from django.contrib.auth import get_user_model
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, View
+from django.views.generic import CreateView, ListView, View
 from posts.models import Post
 
 from .forms import CreationForm, ProfileForm
@@ -10,15 +11,34 @@ User = get_user_model()
 
 
 class SignUp(CreateView):
+    """Класс представление регистрации пользователей."""
+
     form_class = CreationForm
     success_url = reverse_lazy("posts:index")
     template_name = "users/signup.html"
 
 
+class UserUpdateView(View):
+    """Класс представление для редактирования профиля пользователя."""
+
+    def get(self, request):
+        form = ProfileForm(instance=request.user)
+        context = {"form": form}
+        return render(request, "users/profile_form.html", context)
+
+    def post(self, request):
+        form = ProfileForm(request.POST, request.FILES, instance=request.user)
+        if form.is_valid():
+            form.save()
+        context = {"form": form}
+        return render(request, "users/profile_form.html", context)
+
+
 class ProfileView(View):
+    """Класс представление личного кабинета пользователя."""
+
     def get(self, request):
         context = {
-            "form": ProfileForm(instance=request.user),
             "view_posts": Post.objects.filter(view_posts__user=request.user).order_by(
                 "-view_posts__date_of_viewing"
             ),
@@ -28,9 +48,22 @@ class ProfileView(View):
         }
         return render(request, "users/me.html", context)
 
-    def post(self, request):
-        user = User.objects.get(id=request.user.id)
-        form = ProfileForm(request.POST, instance=user, files=request.FILES or None)
-        if form.is_valid():
-            form.save()
-        return render(request, "users/me.html", {"user": user, "form": form})
+
+class UserListView(ListView):
+    """Класс представление списка всех пользователей."""
+
+    model = User
+    paginate_by = 8
+
+
+class SubscriptionListView(LoginRequiredMixin, ListView):
+    """Класс представление подписок пользователей."""
+
+    model = User
+    paginate_by = 8
+
+    def get_queryset(self):
+        queryset = User.objects.filter(
+            following__user__username=self.request.user.username
+        )
+        return queryset
