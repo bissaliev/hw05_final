fa78520de4b38ddfa577353fc2415bee7efdaf33
bissaliev <bissaliev21@gmail.com>
2024-08-30 16:43:17 +0400

    Добавил verbose_name для приложений posts, users

--- yatube/posts/apps.py
+++ yatube/posts/apps.py
@@ -3,6 +3,7 @@ from django.apps import AppConfig
 
 class PostsConfig(AppConfig):
     name = "posts"
+    verbose_name = "Публикации"
 
     def ready(self) -> None:
-        import posts.signals
+        import posts.signals  # noqa
